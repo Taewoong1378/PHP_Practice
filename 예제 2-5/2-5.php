5c62e0bcb85be3615cc2f144984c1b0b6dc3c192
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-09-14 00:37:11 +0900

    initial commit

---
+++ 예제 2-5/2-5.php
@@ -0,0 +1,5 @@
+<?php
+if($_POST['email']=='ktw2378@naver.com'){
+    print "환영합니다 강태웅님!";
+}
+?>
---
+++ 예제 2-6/2-6.php
@@ -0,0 +1,5 @@
+<?php
+if((strcasecmp($_POST['email'])=='ktw2378@naver.com')==0){
+    print "다시 만나뵙게 되어 반갑습니다 강태웅님!";
+}
+?>
